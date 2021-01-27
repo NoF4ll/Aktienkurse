a6c99532031ee4bac6f4c0a982f5d35f9b5c90fe
NoF4ll <55537941+NoF4ll@users.noreply.github.com>
2021-01-27 14:42:51 +0100

    Final Updload

---
+++ src/DatabaseManager.java
@@ -0,0 +1,101 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class DatabaseManager {
+	private static DatabaseManager instance;
+
+	public static DatabaseManager getInstance() {
+		if (instance != null) {
+			return instance;
+		}
+		instance = new DatabaseManager();
+		return instance;
+	}
+
+	public Connection getDatabaseConnection(final int port, final String database) throws SQLException {
+		System.out.println(String.format("[mysql]: Connecting to MySQL:%s - %s", port, database));
+		return DriverManager.getConnection(
+				String.format("jdbc:mysql://localhost:%s/%s?serverTimezone=UTC&useSSL=false", port, database), "root",
+				"bichl601");
+	}
+
+	public void releaseConnection(final Connection connection) throws SQLException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+
+	public void insertIntoDatabase(final Connection connection, LocalDate date, double closeValue, String aktie)
+			throws SQLException {
+		String createTable = "CREATE TABLE IF NOT EXISTS " + aktie
+				+ " ( Datum varchar(50), close varchar(10), PRIMARY KEY(Datum));";
+		try (PreparedStatement statement = connection.prepareStatement(createTable)) {
+			statement.execute(createTable);
+			statement.executeUpdate();
+		}
+		final String insertInto = "insert ignore into " + aktie + " (Datum, close) values (?, ?)";
+		try (PreparedStatement statement = connection.prepareStatement(insertInto)) {
+			statement.setString(1, date.toString());
+			statement.setDouble(2, closeValue);
+			statement.executeUpdate();
+		}
+	}
+
+	public static void insertAVG(final Connection connection, String aktie, double avg, LocalDate date)
+			throws SQLException {
+		String createTable = "CREATE TABLE IF NOT EXISTS " + aktie + "AVG (AVG varchar(50), Datum varchar(50))";
+		try (PreparedStatement statement = connection.prepareStatement(createTable)) {
+			statement.execute(createTable);
+			statement.executeUpdate();
+		}
+		final String insertInto = "replace into " + aktie + "AVG (AVG,Datum) values (?, ?)";
+		try (PreparedStatement statement = connection.prepareStatement(insertInto)) {
+			statement.setDouble(1, avg);
+			statement.setString(2, date.toString());
+			statement.executeUpdate();
+		}
+	}
+
+	public static void selectAvg(String aktie, final Connection connection, ArrayList<Double> closeValue,
+			ArrayList<LocalDate> date) {
+		for (int i = 0; i < date.size(); i++) {
+			String select = "with temp as (select close from " + aktie + " where datum <= '" + date.get(i)
+					+ "' order by datum desc limit 200) select avg(close) from temp";
+			try {
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(select);
+
+				rs.next();
+
+				closeValue.add(rs.getDouble(1));
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public static void selectALL(String aktie, final Connection connection, ArrayList<Double> closeValue,
+			ArrayList<LocalDate> date) {
+		String selectFrom = "SELECT * FROM " + aktie + " ORDER BY DATUM ASC";
+
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(selectFrom);
+
+			while (rs.next()) {
+				date.add(LocalDate.parse(rs.getString("Datum")));
+				closeValue.add(rs.getDouble("close"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,148 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class main extends Application {
+
+
+	static ArrayList<LocalDate> dates = new ArrayList<>();
+	static ArrayList<Double> closeValue = new ArrayList<>();
+
+	static ArrayList<LocalDate> sortetDates = new ArrayList<>();
+	static ArrayList<Double> sortetCloseValue = new ArrayList<>();
+	static ArrayList<Double> sortetAvgCloseValue = new ArrayList<>();
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws MalformedURLException, JSONException, IOException, SQLException {
+		boolean ok = false;
+		System.out.println("Welche Aktie möchten sie bekommen? \n" + "tsla ... Tesla\n" + "AAPL ... Apple.");
+		String aktie = sc.next();
+		final Connection connection = DatabaseManager.getInstance().getDatabaseConnection(3306, "aktien");
+		while(ok == false)
+		{
+			
+			System.out.println("Möchten sie die Datenbank updaten ...1 \noder den Chart Zeichnen ... 2");
+			int a =sc.nextInt();
+			if(a == 1)
+			{
+				updateDatabase(connection, aktie);
+				ok = true;
+			}
+			if(a == 2)
+			{
+				
+				showChart(connection,aktie);
+				ok = true;
+			}
+		}
+		sc.close();
+	}
+
+	public static void updateDatabase(Connection connection, String aktie) throws MalformedURLException, JSONException, IOException, SQLException
+	{
+		APIService.getData(aktie, dates, closeValue);
+	
+		for (int i = 0; i < closeValue.size(); i++) {
+			DatabaseManager.getInstance().insertIntoDatabase(connection, dates.get(i), closeValue.get(i), aktie);
+		}
+		System.out.println("Datenbank wurde erfolgreich updated.");
+	}
+	public static void showChart(Connection connection, String aktie) throws SQLException {
+		DatabaseManager.selectALL(aktie, connection, sortetCloseValue, sortetDates);
+		DatabaseManager.selectAvg(aktie, connection, sortetAvgCloseValue , sortetDates);
+		for (int i = 0; i < sortetAvgCloseValue.size(); i++) {
+			DatabaseManager.insertAVG(connection, aktie, sortetAvgCloseValue.get(i), sortetDates.get(i));
+		}
+		Application.launch();
+		System.out.println("Chart wurde erfolgreich gezeichnet!");
+	}
+
+	static void avgClose() {
+		double firstValue;
+		double sum = 0;
+		int counter = 0;
+
+		for (int i = 0; i < sortetCloseValue.size(); i++) {
+			counter++;
+			if (counter <= 200) {
+				sum = sortetCloseValue.get(i) + sum;
+				sortetAvgCloseValue.add(sum / counter);
+			}
+			if (counter > 200) {
+				firstValue = sortetCloseValue.get(i - 200);
+				sum = sum - firstValue;
+				sum = sum + sortetCloseValue.get(i);
+				sortetAvgCloseValue.add(sum / 200);
+			}
+		}
+	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			final CategoryAxis xAxis = new CategoryAxis();
+			final NumberAxis yAxis = new NumberAxis();
+			xAxis.setLabel("Datum");
+			yAxis.setLabel("close-Wert");
+			
+			final LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
+			lineChart.setTitle("Aktienkurs");
+
+			XYChart.Series<String, Number> tatsaechlich = new XYChart.Series();
+			tatsaechlich.setName("Close-Werte");
+			for (int i = 0; i < sortetCloseValue.size() - 1; i++) {
+				tatsaechlich.getData().add(new XYChart.Data(sortetDates.get(i).toString(), sortetCloseValue.get(i)));
+			}
+			
+			XYChart.Series<String, Number> durchschnitt = new XYChart.Series();
+			durchschnitt.setName("AVG Wert");
+			for (int i = 0; i < sortetAvgCloseValue.size() - 1; i++) {
+				durchschnitt.getData().add(new XYChart.Data(sortetDates.get(i).toString(), sortetAvgCloseValue.get(i)));
+			}
+			Scene scene = new Scene(lineChart, 800, 600);
+			lineChart.getData().add(tatsaechlich);
+			lineChart.getData().add(durchschnitt);
+
+			if (sortetCloseValue.get(sortetCloseValue.size() - 1) >= sortetAvgCloseValue
+					.get(sortetAvgCloseValue.size() - 1)) {
+				scene.getStylesheets().add("green.css");
+			}
+			if (sortetCloseValue.get(sortetCloseValue.size() - 1) < sortetAvgCloseValue.get(sortetAvgCloseValue.size() - 1)) {
+				scene.getStylesheets().add("red.css");
+			}
+
+			lineChart.setCreateSymbols(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
