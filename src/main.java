4b1623694ad5cea2706940700eb1388ca58e6f0d
NoF4ll <55537941+NoF4ll@users.noreply.github.com>
2021-03-17 15:52:39 +0100

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -6,12 +7,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -19,6 +24,7 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.WritableImage;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -28,53 +34,52 @@ import java.time.LocalDate;
 
 public class main extends Application {
 
-
 	static ArrayList<LocalDate> dates = new ArrayList<>();
 	static ArrayList<Double> closeValue = new ArrayList<>();
 
 	static ArrayList<LocalDate> sortetDates = new ArrayList<>();
 	static ArrayList<Double> sortetCloseValue = new ArrayList<>();
 	static ArrayList<Double> sortetAvgCloseValue = new ArrayList<>();
+	
+	static String aktie;
 
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) throws MalformedURLException, JSONException, IOException, SQLException {
 		boolean ok = false;
 		System.out.println("Welche Aktie möchten sie bekommen? \n" + "tsla ... Tesla\n" + "AAPL ... Apple.");
-		String aktie = sc.next();
+		aktie = sc.next();
 		final Connection connection = DatabaseManager.getInstance().getDatabaseConnection(3306, "aktien");
-		while(ok == false)
-		{
-			
+		while (ok == false) {
+
 			System.out.println("Möchten sie die Datenbank updaten ...1 \noder den Chart Zeichnen ... 2");
-			int a =sc.nextInt();
-			if(a == 1)
-			{
+			int a = sc.nextInt();
+			if (a == 1) {
 				updateDatabase(connection, aktie);
 				ok = true;
 			}
-			if(a == 2)
-			{
-				
-				showChart(connection,aktie);
+			if (a == 2) {
+
+				showChart(connection, aktie);
 				ok = true;
 			}
 		}
 		sc.close();
 	}
 
-	public static void updateDatabase(Connection connection, String aktie) throws MalformedURLException, JSONException, IOException, SQLException
-	{
+	public static void updateDatabase(Connection connection, String aktie)
+			throws MalformedURLException, JSONException, IOException, SQLException {
 		APIService.getData(aktie, dates, closeValue);
-	
+
 		for (int i = 0; i < closeValue.size(); i++) {
 			DatabaseManager.getInstance().insertIntoDatabase(connection, dates.get(i), closeValue.get(i), aktie);
 		}
 		System.out.println("Datenbank wurde erfolgreich updated.");
 	}
+
 	public static void showChart(Connection connection, String aktie) throws SQLException {
 		DatabaseManager.selectALL(aktie, connection, sortetCloseValue, sortetDates);
-		DatabaseManager.selectAvg(aktie, connection, sortetAvgCloseValue , sortetDates);
+		DatabaseManager.selectAvg(aktie, connection, sortetAvgCloseValue, sortetDates);
 		for (int i = 0; i < sortetAvgCloseValue.size(); i++) {
 			DatabaseManager.insertAVG(connection, aktie, sortetAvgCloseValue.get(i), sortetDates.get(i));
 		}
@@ -102,6 +107,18 @@ public class main extends Application {
 			}
 		}
 	}
+
+	public void saveAsPng(Scene scene, String path) {
+		WritableImage image = scene.snapshot(null);
+		File file = new File(path);
+		try {
+			ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void start(Stage primaryStage) {
@@ -110,16 +127,17 @@ public class main extends Application {
 			final NumberAxis yAxis = new NumberAxis();
 			xAxis.setLabel("Datum");
 			yAxis.setLabel("close-Wert");
-			
+
 			final LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
 			lineChart.setTitle("Aktienkurs");
+			lineChart.setAnimated(false);
 
 			XYChart.Series<String, Number> tatsaechlich = new XYChart.Series();
 			tatsaechlich.setName("Close-Werte");
 			for (int i = 0; i < sortetCloseValue.size() - 1; i++) {
 				tatsaechlich.getData().add(new XYChart.Data(sortetDates.get(i).toString(), sortetCloseValue.get(i)));
 			}
-			
+
 			XYChart.Series<String, Number> durchschnitt = new XYChart.Series();
 			durchschnitt.setName("AVG Wert");
 			for (int i = 0; i < sortetAvgCloseValue.size() - 1; i++) {
@@ -133,13 +151,17 @@ public class main extends Application {
 					.get(sortetAvgCloseValue.size() - 1)) {
 				scene.getStylesheets().add("green.css");
 			}
-			if (sortetCloseValue.get(sortetCloseValue.size() - 1) < sortetAvgCloseValue.get(sortetAvgCloseValue.size() - 1)) {
+			if (sortetCloseValue.get(sortetCloseValue.size() - 1) < sortetAvgCloseValue
+					.get(sortetAvgCloseValue.size() - 1)) {
 				scene.getStylesheets().add("red.css");
 			}
 
 			lineChart.setCreateSymbols(false);
+			
+
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			saveAsPng(scene, "C:\\Users\\Maximilian Neuner\\Documents\\chartImages\\chart"+aktie+".png");
 
 		} catch (Exception e) {
 			e.printStackTrace();
