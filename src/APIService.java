a62640c006ca35c0e79151c1680a10ced5ee7f85
NoF4ll <55537941+NoF4ll@users.noreply.github.com>
2021-03-17 16:51:32 +0100

    Add files via upload

--- src/APIService.java
+++ src/APIService.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
@@ -11,9 +12,12 @@ import org.json.JSONObject;
 
 public class APIService {
 
-	public static void getData(String aktie,ArrayList<LocalDate> dates,ArrayList<Double> closeValue ) throws MalformedURLException, JSONException, IOException {
+	public static void getData(String aktie, ArrayList<LocalDate> dates, ArrayList<Double> closeValue,
+			HashMap<LocalDate, Integer> splitCoeffecient) throws MalformedURLException, JSONException, IOException {
 
-		String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ aktie + "&outputsize=full&apikey=VTRNJJSSMYW3MPD6";
+		double coefficient = 1.0;
+		String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + aktie
+				+ "&outputsize=full&apikey=VTRNJJSSMYW3MPD6";
 		JSONObject json = new JSONObject(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
 		json = json.getJSONObject("Time Series (Daily)");
 
@@ -21,6 +25,12 @@ public class APIService {
 			dates.add(LocalDate.parse((CharSequence) json.names().get(i)));
 			closeValue.add(json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString())
 					.getDouble("4. close"));
+			if (json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString())
+					.getDouble("8. split coefficient") > coefficient) {
+				coefficient = json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString())
+						.getDouble("8. split coefficient");
+				splitCoeffecient.put(LocalDate.parse((CharSequence) json.names().get(i)), (int) coefficient);
+			}
 		}
 	}
 }
